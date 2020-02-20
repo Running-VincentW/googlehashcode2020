0dd7c673d780341aff5eac6b6ad5c49a9e8f5773
Vincent Wong <vinymwong@gmail.com>
2020-02-20 19:43:38 +0000

    some work done

--- src/main/java/hashcode/Data.java
+++ src/main/java/hashcode/Data.java
@@ -5,33 +5,63 @@
  */
 package hashcode;
 
-import java.util.ArrayList;
+import java.util.*;
 
 /**
  *
  * @author Yek
  */
 public class Data {
-    
     public ArrayList<Library> libraries;
     public int days;
     public Integer[] scores;
-    
+
     public Data(ArrayList<Library> libraries, int days, Integer[] scores){
 	this.days = days;
 	this.libraries = libraries;
 	this.scores = scores;
     }
-    
+}
+
+class Book implements Comparable<Book>{
+    int ID;
+    int score;
+
+    @Override
+    public int compareTo(Book o) {
+        return this.score - o.score;
+    }
+
+    public Book(int ID, int score){
+        this.ID = ID;
+        this.score = score;
+    }
 }
 
 class Library{
-    public Integer[] bookIndexes;
+    public PriorityQueue<Book> books = new PriorityQueue<>();
     public int shippingRate;
+
+    static Set<Book> scannedBooks;
+    private Set<Book> scannedBooksAtLibrary;
     
-    public Library(Integer[] bookIndexes, int shippingRate){
-	this.bookIndexes = bookIndexes;
+    public Library(ArrayList<Book> books, int shippingRate){
+	this.books.addAll(books);
 	this.shippingRate = shippingRate;
+    }
+
+    public int getDistinctNewBooks(){
+        int count = 0;
+        for (Book book : scannedBooks){
+            if (!scannedBooks.contains(book)){
+                count ++;
+            }
+        }
+        return count;
+    }
+
+    public void scanBooks(){
+        this.books.p
     }
     
 }
